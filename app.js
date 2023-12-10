e55dc6a2e7389395a5d88b282a5f3f9d6c099615
revi-hub <150064623+revi-hub@users.noreply.github.com>
2023-12-10 19:09:19 +0100

    Write function that handles rounds

--- app.js
+++ app.js
@@ -120,3 +120,29 @@ function playRound(playerSelection, computerSelection){
         return "Lose"
     }
 }
+
+function game(){
+    let playerWins = 0
+    let computerWins = 0
+    for(let i = 0; i < 5; i++){
+        let playerChoice = getPlayerChoice()
+        let computerChoice = getComputerChoice()
+        let roundResult = playRound(playerChoice, computerChoice)
+        alert(`You picked ${playerChoice} and computer picked ${computerChoice}, It's a ${roundResult}!`)
+        if(roundResult == `Lose`){
+            computerWins += 1
+        } 
+        else if(roundResult == `Win`){
+            playerWins += 1
+        } 
+    }
+    if(playerWins > computerWins){
+        alert(`You won, congratulations! Your score: ${playerWins}; Computer score: ${computerWins}`)
+    }
+    else if(playerWins < computerWins){
+        alert(`You lost, better luck next time! Your score: ${playerWins}; Computer score: ${computerWins}`)
+    }
+    else{
+        alert(`It's a Tie! Your score: ${playerWins}; Computer score: ${computerWins}`)
+    }
+}
